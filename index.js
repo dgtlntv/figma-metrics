2d2b24a4f79f5973bab16db3416f72bd1efe6073
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2024-06-05 13:53:13 +0200

    add some comments

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ async function main() {
     const FIGMA_TEAM_ID = process.env["FIGMA-TEAM-ID"]
     const FIGMA_LIBRARY_FILES = JSON.parse(process.env["FIGMA_LIBRARY_FILES"])
     const figmaApi = new FigmaAPI(API_TOKEN, FIGMA_TEAM_ID)
+    // A map of all components we consider to be part of the Design System and want to track + some metadata of the component
     const componentMap = await figmaApi.getComponentMapFromFiles(FIGMA_LIBRARY_FILES)
     const projects = await figmaApi.getTeamProjects()
     const projectsData = []
@@ -41,12 +42,14 @@ async function main() {
 
             console.log(`Processing file of ${i + 1} of ${projectFiles.length}`)
 
+            // Creating a flat array of all nodes in a file and removing any hidden nodes
             for (const page of fileDocument.children) {
                 const allNodes = findAll(page, () => true)
                 const { nonHiddenNodes } = filterHiddenNodes(allNodes)
                 allNodesInFile.push(...nonHiddenNodes)
             }
 
+            // Process the nodes of the file: check for components from our componentMap, check for detachedComponents
             const { nonComponentNodes, filteredComponentNodes } = processAndFilterNodes(
                 allNodesInFile,
                 allComponentNodes,
@@ -54,6 +57,17 @@ async function main() {
                 componentMap
             )
 
+            const wholeFileStats = calculateStats(
+                allNodesInFile,
+                filteredComponentNodes,
+                nonComponentNodes,
+                detachedComponents
+            )
+
+            // The previous filtering was for the whole Figma file.
+            // While these stats are also interesting we can not really judge someone for explorig with non design system components.
+            // What is important however is the usage of design system components in the final handover.
+            // Thats why we want to also create separate stats for files marked as ready for development.
             const readyForDevNodes = filterReadyForDev(allNodesInFile)
             const {
                 nonComponentNodes: readyForDevNonComponentNodes,
@@ -65,12 +79,6 @@ async function main() {
                 componentMap
             )
 
-            const wholeFileStats = calculateStats(
-                allNodesInFile,
-                filteredComponentNodes,
-                nonComponentNodes,
-                detachedComponents
-            )
             const readyForDevStats = calculateStats(
                 readyForDevNodes,
                 filteredReadyForDevComponentNodes,
